399036ef6600a1ca0149b3f88f28296c2109853c
angadbakshi424 <angadbakshi424@gmail.com>
2026-07-23 20:46:58 +0530

    Header and StudentCard Added

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>Student Course Management System</h1>
+      <p>React version</p>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/StudentCard.jsx
@@ -0,0 +1,11 @@
+function StudentCard() {
+  return (
+    <div>
+      <h3>Aarav Sharma</h3>
+      <p>Course: React Native</p>
+      <p>GPA: 8.4</p>
+    </div>
+  );
+}
+
+export default StudentCard;
